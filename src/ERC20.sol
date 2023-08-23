5bc79e7ca5bdfeb6f73dba7d9839d339d3b8799a
c3phas <mainapeter081@gmail.com>
2023-08-23 13:05:32 +0300

    Initial implementation(non official) of ERC20 Token in Yul

---
+++ src/ERC20.sol
@@ -0,0 +1,232 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.18;
+
+
+contract ERC20{
+/*State variables*/
+
+uint256 private TotalSupply;
+mapping(address => uint256) private balances;
+mapping(address => mapping(address => uint256)) private allowance;
+
+/*Constants and immutables*/
+address  owner;
+
+/*Events*/
+event Transfer(address indexed from, address indexed to, uint256 amount);
+event Approval(address indexed owner, address indexed spender, uint256 amount);
+//0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925
+/*Errors*/
+error mustBeOwner();
+error valueOverFlow();
+error notEnoughBalance();
+error addressZero();
+
+/*onlyOwner() modifier*/
+
+modifier onlyOwner(){
+    assembly{
+        if iszero(eq(caller(),sload(owner.slot))){
+            //store the sig of the error: cast sig mustBeOwner()
+            mstore(0x00,0x5b11cec6)
+            revert(0x1c,0x04)
+        }
+    }
+    _;
+}
+constructor(){
+    owner = msg.sender;
+}
+
+//We need to add some tokens to supply
+//@dev add tokens to supply,only owner can mint tokens
+//@param to The address that receives the initial tokens
+//@param amount The amount to mint(add to market)
+function mint(address to,uint256 amount) external onlyOwner{
+  //Since we use assembly , we have to check for overflows
+  //Minting, will add TotalSupply and also balanceOf the sender
+    assembly{
+        let _totalSupplyBefore := sload(TotalSupply.slot)
+        let _totalSupplyAfter := add(_totalSupplyBefore,amount)
+        if gt(_totalSupplyBefore,_totalSupplyAfter){
+            //we revert with overflow() error
+            mstore(0x00,0x29f4cd11)
+            revert(0x1c,0x04)
+        }
+        //we didn't revert, so add amount to TotalSupply
+        sstore(TotalSupply.slot,_totalSupplyAfter)
+        //we also need to update the balance of the caller()
+        //mapping(address => uint256) balanceOf
+        //For mappings, we need to get the slot then hash it with the key 
+        //let slot := balanceOf.slot
+        mstore(0x00,to)
+        mstore(0x20,balances.slot)
+        let location := keccak256(0x00,0x40)
+        sstore(location,add(sload(location),amount))
+
+        //we have added supply and updated balances, so we now emit an event
+        mstore(0x00,amount)
+        log3(0x00,0x20,0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef,0,to)
+    }
+}
+
+
+//We can mint tokens, let's burn some
+/**Burning is basically removing them from totalSupply and reduce the balance of the burner 
+ * 
+ * 
+*/
+//@dev Function to burn tokens - one can only burn his/her tokens, you cannot burn more than you have
+//@param amount - Amount of tokens to burn
+function burn(uint256 amount) external{
+    assembly{
+        //validate that msg.sender isn't burning more than they have
+        //Get their balance first
+        mstore(0x20,balances.slot)
+        mstore(0x00, caller())
+        let senderBalance := keccak256(0x00,0x40)
+        if lt(sload(senderBalance),amount){
+            mstore(0x00,0x9b834218)
+            revert(0x1c,0x04)
+        }
+        //if allowed, subtract amount from totalSupply
+        sstore(TotalSupply.slot,sub(sload(TotalSupply.slot),amount))
+        //update caller() balance
+        sstore(senderBalance,sub(sload(senderBalance),amount))
+
+        //we have added supply and updated balances, so we now emit an event
+        mstore(0x00,amount)
+        log3(0x00,0x20,0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef,caller(),0)
+    }
+
+
+}
+    //We can mint,burn , let's transfer some to someone else
+    //@dev Transfer tokens from sender to the supplied address, don't allow transfer to zero and you must have enough balance
+    //@param to - the address to whom you wish to transfer tokens to
+    //@param amount - The amount of tokens to transfer
+    //@return true on success , false otherwise - compliant with most tokens
+    function transfer(address to,uint256 amount) external returns(bool){
+        assembly{
+            if iszero(to){
+                mstore(0x00,0x7299a729)
+                revert(0x1c,0x04)
+            }
+            //Get the balance of the sender and compare against amount
+            mstore(0x00,caller())
+            mstore(0x20,balances.slot)
+            let balanceSlot := keccak256(0x00,0x40)
+            if gt(amount,sload(balanceSlot)){
+                mstore(0x00,0x9b834218)
+                revert(0x1c,0x04)
+            }
+            // checks passed, move the funds
+            sstore(balanceSlot,sub(sload(balanceSlot),amount))
+            //update receivers balance
+            mstore(0x00,to)
+            let receiverBalanceSlot := keccak256(0x00,0x40)
+            sstore(receiverBalanceSlot,add(sload(receiverBalanceSlot),amount))
+             
+        //we have added supply and updated balances, so we now emit an event
+        mstore(0x00,amount)
+        log3(0x00,0x20,0xddf252ad1be2c89b69c2b068fc378daa952ba7f163c4a11628f55a4df523b3ef,caller(),0)
+
+        }
+        return true;    
+    }
+
+    //We can transfer to others, but can others transfer on our behalf
+    //We need to first allow them to, so we approve
+    //mapping(address => mapping(address => uint256)) allowance
+    //@dev approve - Allow spender to spend some of our tokens
+    //@param spender - Address to allow
+    //@param amount - Amount to allow them to spend
+    function approve(address spender,uint256 amount) external{
+        assembly{
+            //we can't delegate to address 0
+            if iszero(spender){
+                mstore(0x00,0x7299a729)
+                revert(0x1c,0x04)
+            }
+            //we need to update the allowance mapping
+            //allowance[msg.sender][spender] = amount
+            //Get the locations
+            mstore(0x00,caller())
+            mstore(0x20,allowance.slot)
+            let ownersBalanceSlot := keccak256(0x00,0x40)
+            //For the second map, we hash ownersBalanceSlot with spender
+            mstore(0x00,spender)
+            mstore(0x20,ownersBalanceSlot)
+            let spenderBalanceSlot := keccak256(0x00,0x40)
+            sstore(spenderBalanceSlot,amount)
+            mstore(0x00,amount)
+            log3(0x00,0x20,0x8c5be1e5ebec7d5bd14f71427d1e84f3dd0314c0f7b2291e5b200ac8c7c3b925,caller(),spender)
+        }
+    }
+
+    //We now have approval to spend some tokens
+    //@dev TransferFrom(address from, address to, amount): Note sender is now the spender
+    function transferFrom(address from, address to, uint256 amount) external returns(bool){
+        //Get the allowed amount
+        assembly{
+            //To get allowed we use the allowance[msg.sender][spender]
+            mstore(0x00,from)
+            mstore(0x20,allowance.slot)
+            let ownerSlot := keccak256(0x00,0x40)
+
+            mstore(0x20,ownerSlot)
+            mstore(0x00, caller())
+            let finalSlot := keccak256(0x00,0x40)
+            
+            let allowed := sload(finalSlot)
+            if lt(allowed,amount){
+                mstore(0x00,0x9b834218)
+                revert(0x1c,0x04)  
+            }
+            //Allowed to transfer so update allowance
+            sstore(finalSlot,sub(allowed,amount))
+
+            //Transfer
+            //Reduce amount from the from address
+            mstore(0x00,from)
+            mstore(0x20,balances.slot)
+            let fromSlot := keccak256(0x00,0x40)
+            if gt(amount,sload(fromSlot)){
+                mstore(0x00,0x9b834218)
+                revert(0x1c,0x04)  
+            }
+            sstore(fromSlot,sub(sload(fromSlot),amount))
+            //add amount to the receiver
+            mstore(0x00,to)
+            let toSlot := keccak256(0x00,0x40)
+            sstore(toSlot,add(sload(toSlot),amount))
+
+        }
+        return true;
+    }
+
+    function getAllowance(address spender) external view returns (uint256){
+        return allowance[msg.sender][spender];
+    }
+
+    function balanceOf(address _owner) external view returns (uint256 result){
+        //return balances[_owner];
+        assembly{
+            //get the slots
+            mstore(0x20,balances.slot)
+            mstore(0x00,_owner) 
+            
+            result := sload(keccak256(0x00,0x40))
+
+        }
+    }
+    function totalSupply() external view returns(uint256 _totalSupply){
+        assembly{
+            _totalSupply := sload(TotalSupply.slot)
+
+        }
+    }
+
+
+
+}
---
+++ test/ERC20.t.sol
@@ -0,0 +1,171 @@
+pragma solidity 0.8.18;
+
+import {Test,console} from "forge-std/Test.sol";
+import {console} from "forge-std/console.sol";
+import {ERC20} from "../src/ERC20.sol";
+
+contract ERC is Test{
+    ERC20 erc20token;
+    address ZERO = address(0);
+    address JANE = address(1);
+    address BOB = address(2);
+    address ALICE = address(3);
+
+    function setUp() public{
+        erc20token = new ERC20();
+    }
+
+    function testTotalSupply() public{
+        //Test before mints
+        assertEq(erc20token.totalSupply(),0,"Total supply should be zero before mints");
+    }
+
+    function testNotOwnerMints() public{
+        //The function mint should revert if called by someone who is not owner
+        vm.expectRevert(0x5b11cec6);
+        vm.startPrank(ALICE);
+        erc20token.mint(BOB,50);
+        vm.stopPrank();
+
+    }
+
+    function testOwnerMints() public{
+        erc20token.mint(BOB,30);
+        //After Mints, totalsupply should increase and Bob balance to
+        assertEq(erc20token.totalSupply(),30,"Total supply should be zero before mints");
+        assertEq(erc20token.balanceOf(BOB),30,"Bob's balace should be amount minted");
+       
+    }
+    //What happens if we mint more than type(uint256).max
+    function testMintOverflows() public{
+        vm.expectRevert();
+        erc20token.mint(BOB,type(uint256).max + 1);
+    }
+    function testTransfer() public {
+        erc20token.mint(BOB,30);
+        //test the transfer
+        assertEq(erc20token.balanceOf(ALICE),0,"Alice's balance before transfer ");
+        //Pretend to be BOB in order to make transfers
+        vm.prank(BOB);
+        erc20token.transfer(ALICE,15);
+        //Check status for variables affected after the transfer
+        assertEq(erc20token.totalSupply(),30,"Total supply affected by transfers");
+        assertEq(erc20token.balanceOf(BOB),15,"Bob's balance should be amount after transfer");
+        assertEq(erc20token.balanceOf(ALICE),15,"Alice's balance should be amount transfered to her plus any other balance she had");
+    }
+
+    function testTransferToZeroAddress() public{
+        erc20token.mint(BOB,30);
+        //test the transfer
+        //We expect to revert with the error selector for addressZero()
+        vm.expectRevert(0x7299a729);
+        vm.prank(BOB);
+        erc20token.transfer(ZERO,15);
+
+    }
+
+    function testTransferMoreThanOurBalance() public{
+        erc20token.mint(BOB,30);
+        //test the transfer
+        //We expect to revert with the error selector for notEnoughBalance()
+        vm.expectRevert(0x9b834218);
+        vm.prank(BOB);
+        erc20token.transfer(ALICE,35);
+    }
+
+    function testApprove() public{
+        //Mint to alice
+        erc20token.mint(ALICE,30);
+        vm.startPrank(ALICE);
+        //Alice wants bob to be able to spend some of her tokens
+        erc20token.approve(BOB,10);
+        //console.log(erc20token.getAllowance(BOB));
+        assertEq(erc20token.getAllowance(BOB),10,"Allowance not correct");
+        vm.stopPrank();
+    }
+
+    function testApproveToZeroAddress() public{
+        //Mint to alice
+        erc20token.mint(ALICE,30);
+        vm.startPrank(ALICE);
+        //Alice wants approve ZERO address, which should revert
+        vm.expectRevert(0x7299a729);
+        erc20token.approve(ZERO,10);
+        vm.stopPrank();
+    }
+
+    function testTransferFrom() public {
+        erc20token.mint(BOB,30);
+        //After Mints, totalsupply should increase and Bob balance to
+        assertEq(erc20token.totalSupply(),30,"Total supply should be zero before mints");
+        assertEq(erc20token.balanceOf(BOB),30,"Bob's balace should be amount minted");
+        vm.startPrank(BOB);
+        //Approve Alice to spend some of Bob's tokens
+        erc20token.approve(ALICE,25);
+        //console.log(erc20token.getAllowance(BOB));
+        assertEq(erc20token.getAllowance(ALICE),25," First Allowance not correct");
+        assertEq(erc20token.balanceOf(ALICE),0,"Alice's balance before and after approve should not change");
+        assertEq(erc20token.balanceOf(JANE),0,"Jane's balance before the transfer not correct");
+        vm.stopPrank();
+
+        //WE now test if alice can actually transfer on behalf of BOB
+        vm.prank(ALICE);
+        erc20token.transferFrom(BOB,JANE,20);
+        assertEq(erc20token.balanceOf(ALICE),0,"Alice's balance should be zero");
+        assertEq(erc20token.balanceOf(JANE),20,"Jane's balance should be amount transferedTo");
+        assertEq(erc20token.balanceOf(BOB),10,"Bob's balance should be amount minted - transferedTo");
+        vm.prank(BOB);
+        assertEq(erc20token.getAllowance(ALICE),5," Second Allowance not correct");
+    }
+
+    function testTransferMoreThanAllowance() public {
+        erc20token.mint(BOB,30);
+        vm.prank(BOB);
+        //Approve Alice to spend some of Bob's tokens
+        erc20token.approve(ALICE,20);
+        //Alice attempts to transfer more than 20 : we expect it to revert with notEnoughBalance()
+        vm.expectRevert(0x9b834218);
+        vm.prank(ALICE);
+        erc20token.transferFrom(BOB,JANE,25);
+
+    }
+    //Since approval doesn't check how much the owner has before approving, what happens if they approve more than they have
+    //Should we revert during the approval period or wait for a transfer to be initiated
+    function testTransferMoreThanOwnerBalance() public{
+        erc20token.mint(BOB,30);
+        vm.prank(BOB);
+        //Approve Alice to spend some of Bob's tokens
+        erc20token.approve(ALICE,35);
+        //Alice attempts to transfer more than everything approved to her : we expect it to revert with notEnoughBalance()
+        vm.expectRevert(0x9b834218);
+        vm.prank(ALICE);
+        erc20token.transferFrom(BOB,JANE,35); 
+    }
+
+    function testTransferFromWhenNotApproved() public{
+        erc20token.mint(BOB,30);
+        vm.expectRevert(0x9b834218);
+        vm.prank(ALICE);
+        erc20token.transferFrom(BOB,JANE,20);
+    }
+    function testBurn() public{
+        //Burning should reduce total supply and also msg.senders balance
+        erc20token.mint(BOB,30);
+        assertEq(erc20token.totalSupply(),30, "Supply before burning");
+        vm.prank(BOB);
+        erc20token.burn(20);
+        assertEq(erc20token.totalSupply(),10, "Supply After burning");
+        assertEq(erc20token.balanceOf(BOB),10, "Bob's balance after burning");
+
+    }
+    function testBurnMoreThanBalance() public{
+        erc20token.mint(BOB,30);
+        assertEq(erc20token.balanceOf(BOB),30, "Balance of Bob before burning");
+        //If Bob tries to burn more than they have , we expect a revert
+        vm.expectRevert(0x9b834218);
+        vm.prank(BOB);
+        erc20token.burn(35); 
+    }
+    
+}
+
